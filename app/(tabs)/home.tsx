dc62499a0b2eae84be3564588c585b5e8474afbf
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-07-18 21:32:36 +0700

    Fix scrollview

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -29,29 +29,27 @@ const Home = () => {
   return (
     <SafeAreaView className="h-full bg-white">
         <View className="px-6 py-16">
-          <View>
-            <Text className="text-3xl text-black font-black mb-8">
-              Hari ini
-            </Text>
+          <Text className="text-3xl text-black font-black mb-8">
+            Hari ini
+          </Text>
 
-            <View className='flex-row justify-between mb-4'>
-              {days.map(day => (
-                <View key={day.id} className={`items-center px-3 py-2 rounded ${day.name === 'Kam' ? 'bg-amost-primary' : ''}`}>
-                  <Text className={`font-bold text-xs ${day.id === 4 ? 'text-white' : 'text-amost-secondary-dark_2'}`}>{day.name}</Text>
-                  <Text className={`font-bold text-base ${day.id === 4 ? 'text-white' : 'text-black'}`}>{day.date}</Text>
-                </View>
-              ))}
-            </View>
+          <View className='flex-row justify-between mb-6'>
+            {days.map(day => (
+              <View key={day.id} className={`items-center px-2.5 py-2 rounded-lg ${day.name === 'Kam' ? 'bg-amost-primary' : ''}`}>
+                <Text className={`font-bold text-xs ${day.id === 4 ? 'text-white' : 'text-amost-secondary-dark_2'}`}>{day.name}</Text>
+                <Text className={`font-bold text-base ${day.id === 4 ? 'text-white' : 'text-black'}`}>{day.date}</Text>
+              </View>
+            ))}
           </View>
 
-          <ScrollView className='py-6 h-full'>
+          <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
             {meds.map(med => (
               <View key={med.id} className='p-2'>
                 <View className='flex-row items-center mb-1.5'>
                   <Text className='max-w-10 font-medium'>{med.time}</Text>
                   <View className="flex-1 border-dashed border-t border-black ml-4"/>
                 </View>
-                <View className='flex-row justify-between ml-12 bg-amost-secondary-light_1 p-4 rounded-md items-center border border-amost-primary'>
+                <View className='flex-row justify-between ml-12 bg-amost-secondary-light_1 p-4 rounded-lg items-center border border-amost-primary'>
                   <View>
                     <Text className='text-xl font-bold text-black'>{med.name}</Text>
                     <Text className='text-sm font-semibold text-amost-primary'>{med.dose}</Text>
@@ -61,7 +59,7 @@ const Home = () => {
                   </View>
                 </View>
               </View>
-            ))}
+             ))}
           </ScrollView>
         </View>
     </SafeAreaView>
